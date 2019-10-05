31c4441dbe56acb3e503095e44c29a1fa5bdfaca
jfkonecn <jfkonecn@gmail.com>
2019-10-05 12:08:13 -0400

    Added sample recipe data

--- src/app/recipes/view-recipe/view-recipe.component.ts
+++ src/app/recipes/view-recipe/view-recipe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Recipe } from 'src/app/model/recipe';
+import { of, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-view-recipe',
@@ -8,13 +10,44 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ViewRecipeComponent implements OnInit {
 
+  recipe$ : Observable<Recipe> ;
   constructor(
     private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit() {
     const name = this.route.snapshot.paramMap.get('name');
-    console.error(name);
+    const recipe : Recipe = 
+    {
+      name: name,
+      level: 'easy',
+      prepTime: '50 mins',
+      inactiveTime: '10 mins',
+      cookTime: '20 mins',
+      yield: '4 things',
+      ingredients: [
+        '5 eggs',
+        '10 things'
+      ],
+      notes: [
+        'eat when hot'
+      ],
+      directions: [
+        {
+          priority: 1,
+          direction: 'Sleep'
+        },
+        {
+          priority: 2,
+          direction: 'Pet Pigs'
+        }
+      ],
+      tags:[
+        'Pigs',
+        'Otters'
+      ]
+    }
+    this.recipe$ = of(recipe);
   }
 
 }
